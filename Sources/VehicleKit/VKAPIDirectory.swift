341753555cf36a5a7c71d5cf83722155e54082e2
Piper McCorkle <contact@piperswe.me>
2022-04-08 00:12:47 -0500

    Add name to VKAPIDirectory

--- Sources/VehicleKit/VKAPIDirectory.swift
+++ Sources/VehicleKit/VKAPIDirectory.swift
@@ -3,11 +3,12 @@ import Foundation
 public class VKAPIDirectory {
     public struct API {
         let type: VKVehicleAPI.Type
-        let make: String
+        let name: String
+        let makes: Set<String>
     }
 
     public static let apis = [
-        API(type: VKTeslaAPI.self, make: "Tesla"),
-        API(type: VKBMWConnectedDriveAPI.self, make: "BMW")
+        API(type: VKTeslaAPI.self, name: "Tesla", makes: ["Tesla"]),
+        API(type: VKBMWConnectedDriveAPI.self, name: "BMW Connected Drive", makes: ["BMW", "Mini"])
     ]
 }
