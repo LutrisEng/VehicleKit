19e8f7d1932f6f70b0204819ae266900d4a2c3b3
Piper McCorkle <contact@piperswe.me>
2022-04-08 00:14:43 -0500

    Add functions for matching make to API

--- Sources/VehicleKit/VKAPIDirectory.swift
+++ Sources/VehicleKit/VKAPIDirectory.swift
@@ -2,13 +2,30 @@ import Foundation
 
 public class VKAPIDirectory {
     public struct API {
-        let type: VKVehicleAPI.Type
-        let name: String
-        let makes: Set<String>
+        public let type: VKVehicleAPI.Type
+        public let name: String
+        public let makes: Set<String>
+
+        public func matches(make: String) -> Bool {
+            let normalized = make.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+            for potentialMatch in makes {
+                let normalizedPotentialMatch = potentialMatch
+                    .lowercased()
+                    .trimmingCharacters(in: .whitespacesAndNewlines)
+                if normalizedPotentialMatch == normalized {
+                    return true
+                }
+            }
+            return false
+        }
     }
 
     public static let apis = [
         API(type: VKTeslaAPI.self, name: "Tesla", makes: ["Tesla"]),
         API(type: VKBMWConnectedDriveAPI.self, name: "BMW Connected Drive", makes: ["BMW", "Mini"])
     ]
+
+    public static func apis(forMake: String) -> [API] {
+        apis.filter { api in api.matches(make: forMake) }
+    }
 }
